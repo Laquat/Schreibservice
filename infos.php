928805dcfd812ac1220f52eee8f6ee86228c2c8e
Peejey <philipp.janzen@web.de>
2015-06-30 18:52:10 +0200

    Create infos.php

---
+++ infos.php
@@ -0,0 +1,49 @@
+<?php
+include "header.php";
+?>
+	<nav id="loginmenue">
+			<ul class="vertikalalign">
+					<li><a href="index.php"><img id="pfeil" src="images/pfeil.gif" alt="zurück"> Homepage</a></li>
+			</ul>
+	</nav>
+	<nav id="hauptmenue">
+		<ul class="vertikalalign">
+			<li><a href="infos.php#datenschutz">Datenschutz</a></li>
+			<li><a href="infos.php#impressum">Impressum</a></li>
+			<li><a href="infos.php#agb">AGB </a></li>
+		</ul>
+	</nav>
+</header>
+<main class="shadow">
+	<div class="zwischenbox" id ="zwischenbox2"></div>
+	<section class="section_size" id="datenschutz">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Datenschutz</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau .php ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>	
+	<section class="section_size" id="impressum">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Impressum</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau .php ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+	<section class="section_size" id="agb">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>AGB</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau .php ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+</main>
+<?php
+include "footer.php";
+?>
