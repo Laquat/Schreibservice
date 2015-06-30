813e8f12408c29111952cd9f324c034e9e67ecb4
Peejey <philipp.janzen@web.de>
2015-06-30 18:51:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+@include "header.php";
+?>
+	<nav id="loginmenue">
+			<ul class="vertikalalign">
+				<li><a href="login.php">Login & Upload</a></li>
+			</ul>
+	</nav>
+	<nav id="hauptmenue">
+		<ul class="vertikalalign">
+			<li><a href="index.php#schreibbuero">Schreibbüro</a></li>
+			<li><a href="index.php#preise">Leistungen & Preise</a></li>
+			<li><a href="index.php#service">Service </a></li>
+			<li><a href="index.php#kontakt">Kontakt</a></li>
+		</ul>
+	</nav>
+</header>
+<main class="shadow">
+	<section class="section_size" id="schreibservice">
+		<div id="bild">
+			<img src="images/schreibbuero.jpg" alt="Schreibbüro">
+		</div>
+		<div class="zwischenbox" id ="zwischenbox"></div>
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Schreibservice ANNA DICK</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau draußen ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+	<section class="section_size" id="schreibbuero">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Schreibbüro</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau draußen ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+	<section class="section_size" id="preise">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Leistungen & Preise</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau draußen ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+	<section class="section_size" id="service">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Service</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau draußen ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+	<section class="section_size" id="kontakt">
+		<article>
+			<br><br><br><br><br><br><br><br><h1>Kontakt</h1>
+			<p>Wenn die Sonne nicht scheint und es kalt und grau draußen ist, dann
+				kann es uns schon schwer fallen uns zu freuen. Wir haben wieder viel zu
+				viele Rechnungen zu zahlen und viel zu wenig Geld und wir werden
+				unzufrieden und lassen uns von solchen dingen runterziehen.</p>
+		</article>
+	</section>
+</main>
+<?php
+@include "footer.php";
+?>
