d24d23f71e413c276620586d7016dbc1b584821d
Peejey <philipp.janzen@web.de>
2015-08-20 23:19:19 +0200

    komplette umstrukturierung
    komplette umstrukturierung und neue Anmelde und Loginseiten

---
+++ neuanmelden.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Schreibservice Dick</title>
+
+<html lang="de">
+<meta charset="UTF-8">
+<meta http-equiv="content-type" content="text/html"; charset="UTF-8">
+<meta name="robots" content="index, follow">
+<meta name="author" content="WPFMW Projektgruppe">
+<meta name="creator" content="WPFMW Projektgruppe">
+<meta name="publisher" content="WPFMW Projektgruppe">
+<meta name="description" content="Hier kommt die Beschreibung rein">
+
+<link href="css/design_neuanmelden.css" type="text/css" rel="stylesheet" title"Haupttemplate">
+
+</head>
+<body>
+<div id="wrapper">
+<header>
+	<div id="kopf">
+		<a href="index.php"><img class="vertikalalign" src="images/logo.jpg" alt="Logo"></a>
+	</div>
+	<div id="oberbox">
+	</div>
+</header>
+<main>
+<br><h1>Neu Anmelden</h1><br><br>
+<div id="anmeldeformular">
+<form action="index.php" method="post">
+	<label>Name/Firma</label>
+	&nbsp;<input class="textinput" type="text" name="name" value=""/>
+	<br><br><label>Email</label>
+	<input class="textinput" type="text" name="email" value=""/>	
+	<br><br><br>
+	<span>Schon angemeldet? Hier <i><a href="einloggen.php">einloggen</a></i></span>
+	<input class="submit" type="submit" value="weiter">
+</form>
+</div>
+</main>
+<footer>
+</footer>
